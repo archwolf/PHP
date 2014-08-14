6fa7e06895079b62794c022786da7db27015e0da
archwolf <archwolf@users.noreply.github.com>
2014-08-14 19:45:16 +0300

    Create 1.PersonalInfo.php

---
+++ Syntax/1.PersonalInfo.php
@@ -0,0 +1,7 @@
+<?php
+    $firstName = "Arch";
+    $lastName = "Stefanov";
+    $age = 25;
+    $fullName = $firstName. " ". $lastName;
+    echo "My name is $fullName and I am $age years old."
+?>
